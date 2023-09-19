ccd9ffddb306718389614bcd3f30142bb5907ad9
Swapnil <rjswapnil1234@gmail.com>
2023-09-19 20:01:15 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right="0";
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyEZrIPcBuh6uYkRl_8HTxejEbepQ1Qba2FL4-c8g8xaTSS-UuaOH0Ht2t3IryL14c/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
